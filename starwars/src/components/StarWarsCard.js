39ad55db120d073c5d0bde8c7507edd0470a9b45
Aaron Williams <aaronrw4@yahoo.com>
2019-09-27 20:13:30 -0700

    Added styled components

--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -1,10 +1,22 @@
 import React, {useState} from 'react';
+import styled from 'styled-components';
+
+const Card = styled.div`
+    width: 30%;
+    display: flex;
+    flex-direction: column;
+    margin: 10px;
+    background-color: #3E3739;
+    color: #228CD1
+`;
 
 export default function StarWarsCard(props) {
 
     return (
-        <div>
+        <Card>
             <h2>{props.name}</h2>
-        </div>
+            <p>Description: {props.gender}, {props.height}cm, {props.mass}kg.
+            Has {props.eyes} eyes, {props.hair} hair, and {props.skin} skin.</p>
+        </Card>
     )
 }
--- starwars/src/components/StarWarsList.js
+++ starwars/src/components/StarWarsList.js
@@ -1,6 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import StarWarsCard from './StarWarsCard'
+import StarWarsCard from './StarWarsCard';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+`;
 
 export function StarWarsList() {
     const [data, setData] = useState([]);  
@@ -20,14 +27,20 @@ export function StarWarsList() {
   }, []);
 
   return (      
-    <div>
+    <Container>
         {data.map(people => {
             return(
                 <StarWarsCard
                     name={people.name}
+                    gender={people.gender}
+                    height={people.height}
+                    mass={people.mass}
+                    hair={people.hair_color}
+                    skin={people.skin_color}
+                    eyes={people.eye_color}
                 />
             );
         })}
-    </div>
+    </Container>
   );
 }
